429dc5098bdc5a012e4a483591fffea81a52a93d
hongbo_glb <499704147_glb@qq.com>
2020-07-16 11:38:29 +0800

    创建分支

--- src/main/java/cn/itcast/test/Git.java
+++ src/main/java/cn/itcast/test/Git.java
@@ -8,6 +8,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("更新1");
+        System.out.println("创建分支");
     }
 }
 
