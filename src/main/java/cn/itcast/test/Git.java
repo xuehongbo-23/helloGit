2976bb28ffa3b8b1ba789beaf55cc5b4b30a5345
xuehongbo-23 <66169318+xuehongbo-23@users.noreply.github.com>
2020-07-16 12:21:42 +0800

    在线更新

--- src/main/java/cn/itcast/test/Git.java
+++ src/main/java/cn/itcast/test/Git.java
@@ -9,7 +9,7 @@ public class Git {
         System.out.println("hello git");
         System.out.println("更新1");
         System.out.println("创建分支");
-        System.out.println("github创建");
+        System.out.println("github创建1");
     }
 }
 
